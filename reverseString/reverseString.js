15b35053456d1c910215604b02ce8da986d7b8c5
Minku Singh <mailminkusingh@gmail.com>
2021-07-02 21:24:42 +0530

    modified reverseString js

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+const reverseString = function(word) {
 
+    if(word == " ")
+    {
+        return " ";
+    }
+
+    let word_size = word.length;
+    let ans = "";
+    for(let i = word_size-1; i >= 0; i--)
+    {
+        ans += word[i];
+    }
+    return ans;
 };
 
 module.exports = reverseString;
