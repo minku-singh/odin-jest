e85d5e68465dd7e3049f170d140232a15c366c0b
Minku Singh <mailminkusingh@gmail.com>
2021-07-02 21:09:46 +0530

    modified repeatString js file

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,21 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
+    if(times < 0)
+    {
+        return "ERROR";
+    }
 
+    if(word == " ")
+    {
+        return " ";
+    }
+
+    let ans = "";
+    while(times)
+    {
+        ans += word;
+        times--;
+    }
+    return ans;
 };
 
 module.exports = repeatString;
